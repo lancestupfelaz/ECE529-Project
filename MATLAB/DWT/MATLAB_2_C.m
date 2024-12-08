79b5f494bdf999c390f39f91bf1ffe4eec155f5e
lancestupfelaz <lancestupfel@arizona.edu>
2024-12-08 15:04:37 -0700

    working

--- MATLAB/DWT/MATLAB_2_C.m
+++ MATLAB/DWT/MATLAB_2_C.m
@@ -1,40 +1,82 @@
 clear
 close all
-% im=imread('C:/Users/lstup.LANCE/source/repos/ECE529/ECE529-Project/Data/Checkerboard_pattern.png');
-% grayScaleImg = double(im);
 
 %% Input Image
 %im=imread('../../Data/checkerboard_1024x1024','png');
-im=imread('../../Data/dog_1024x1024','png');
-grayScaleImg = rgb2gray(im);
+%im=imread('../../Data/dog_1024x1024','png');
+%im=imread('../../Data/AbrahamLincoln_1024x1024','png');
+im=imread('../../Data/women_512x512','png');
 
+% convert color image to gray scale.
+grayScaleImg = rgb2gray(im);
+figure; imshow(grayScaleImg)
+
+% levels = [1,2,3,4,5,6];
+% thresholds = [1 0.5 0.1 0.05 0.01 0.005 0.001 ];
+% mse = zeros(length(levels), length(thresholds));
+% cr = zeros(length(levels), length(thresholds));
+% 
+% i = 1;
+% for level = levels
+%    j = 1;
+%    for threshold = thresholds
+%        [mse(i,j) cr(i,j)] = waveletProcessing(grayScaleImg, level, threshold);
+%        j = j + 1;
+%    end
+%    i = i +1;
+% end
+% figure; imagesc((mse)); colorbar; ylabel('level'); xlabel('threshold')
+% figure; imagesc((cr)); colorbar; ylabel('level'); xlabel('threshold')
+% 
+
+waveletProcessing(grayScaleImg, 5, 0.015 );
+
+%% wavelet transform parameters
+function [mse,cr] = waveletProcessing(grayScaleImg, levels, threshold)
+
+% image size calculation
 InputImage_bytes = size(grayScaleImg,1) * size(grayScaleImg,2);
 disp('Input image size: ' + string(InputImage_bytes/1000) + '[kb]');
 
-figure; image(grayScaleImg);
+%levels = 3;
+% haar wavelet
 
-%% wavelet transform 2D haar
-% lets use symetic padding
-img = padarray(grayScaleImg, [1 1],"symmetric");
-%img = grayScaleImg;
-levels = 2;
-[M, D] = customDWT(grayScaleImg, levels);
-%[~, H2, V2, D2] = customDWT(A1);
-%figure; imagesc(A1);
+% compute wavelet coefficents DWT
+[waveletCoefficents, waveletCoefficentSizes] = customDWT(grayScaleImg, levels);
+
+% Thresholding
+waveletCoefficents = thresholdSignal(waveletCoefficents, threshold);
+
+% Entropy encoding
+encodedIdxs = (differenceEncode(waveletCoefficents));
+nonZeroCoeff = waveletCoefficents(waveletCoefficents ~= 0);
+
+% Quantization 8-bit
+[quantizedCoefficents, dr, minInput] = customQuantize(nonZeroCoeff);
+
+% measure size
+Idx_size_bytes = length(encodedIdxs) * 2;
+coefficent_size_bytes = length(quantizedCoefficents) * 1;
+total_txed_size_bytes = Idx_size_bytes + coefficent_size_bytes;
+disp('transmitted image size: ' + string(total_txed_size_bytes/1000) + '[kb]');
+
+% Dequantization 8-bit
+[dequantizedCoefficents] = inverseCustomQuantize(quantizedCoefficents, dr, minInput);
 
 
-%dec1 = [A1 H1; V1 D1];
-%figure; image(dec1);
+% entropy decoding
+recoveredCoeffcients = reconstructCoefficentMatrix(encodedIdxs, dequantizedCoefficents, length(waveletCoefficents));
 
-S = uint8(customIDWT(M, D, levels));
-figure; image(S);
-figure; imagesc(grayScaleImg - S)
-mse = mse_image(grayScaleImg, S);
+% recover image using IDWT
+recoveredImage = uint8(customIDWT(recoveredCoeffcients, waveletCoefficentSizes, levels));
+figure; imshow(recoveredImage)
 
-
+% print MSE
+mse = mse_image(grayScaleImg, recoveredImage);
+cr  = InputImage_bytes / total_txed_size_bytes;
 disp("Mean Squared Error: " + num2str(mse));
-
-
+disp("compression ratio " + num2str(cr));
+end
 
 %% Helper functions %%
 % MSE
@@ -42,15 +84,69 @@ function mse = mse_image(imageA, imageB)
     mse = mean((double(imageA) - double(imageB)).^2,"all");
 end
 
-%         256         169   approx
-%         256         169   level 2
-%         511         338   level 1
-%        1022         676   original image
+%% Thresholding
+function output = thresholdSignal(input, threshold)
+    sorted_signal = sort(input);
+    startIdx = (1-threshold)*length(sorted_signal);
+    if(startIdx ~= 0)
+        thresholdValue = sorted_signal(floor(startIdx));
+        keep_idxs = abs(input) > thresholdValue;
+        output = input .* keep_idxs;
+    else
+        output = input;
+    end
+end
 
-%    256*169*4+511*338*3 = 691210
+%% Quantization
+function [output, dr, minInput] = customQuantize(input)
+    % shift numbers to be positive
+    %figure; histogram(input(0~=input))
 
-%    691210
-% IDWT Functions 
+    minInput = min(input);
+    positiveInput = input + -minInput;
+    
+    % quantize to 8 bits linearly
+    dr = max(positiveInput);
+    output = uint8(round( 255 * positiveInput / dr ));
+    %figure; histogram(output(52~=output))
+
+end
+
+function [output] = inverseCustomQuantize(input, dr, minInput)
+    positiveInput = dr * double(input) / 255;
+    %figure; histogram(positiveInput(median(positiveInput)~=positiveInput))
+
+    % shift numbers back
+    output = positiveInput + minInput;
+    %figure; histogram(output(median(output)~=output))
+
+end
+
+%% Entropy
+
+% encoding
+function [encodedDifferences] = differenceEncode(data)
+    % Find the indices of non-zero elements
+    nonZeroIndices = find(data ~= 0);
+    
+    encodedDifferences = diff(nonZeroIndices);
+    encodedDifferences = [nonZeroIndices(1); encodedDifferences];
+end
+
+function M = reconstructCoefficentMatrix(indices, data, sz)
+    decodedIdxs = differenceDecode(indices);
+    
+    M = zeros(sz,1);
+    M(decodedIdxs) = data;
+end
+
+% decoding
+function decodedIndices = differenceDecode(encodedDifferences)
+    decodedIndices = cumsum(encodedDifferences);
+end
+
+
+%% IDWT Functions 
 % input is stored as [A, Hn, Vn, Dn, ... , H1, V1, D1]
 function S = customIDWT(M, Book, levels)
 lastIdx = 1;
@@ -66,13 +162,8 @@ for l=levels:-1:1
 
     if( l == levels) % base case
         A = reshape(M(lastIdx:coef_size+lastIdx - 1), [rows cols]);  lastIdx = lastIdx + coef_size;
-        %A(:,size(A,2)) = [];
-        Rv = 'same';
-        Cv = 'same';
     else 
         A = S;
-        Rv = 'same';
-        Cv = 'same';
     end
     %figure; imagesc(A);
 
@@ -83,19 +174,15 @@ for l=levels:-1:1
     lastIdx = lastIdx + coef_size;
 
     % reconstructed signal is A + Detail_1 + ... + Detail_n
-    A_rec = interpolateColumnsAndLowPass(A,'same');
-    %figure; imagesc(A_rec);
-    H_rec = interpolateColumnsAndHighPass(H,'same');
+    A_rec = interpolateColumnsAndLowPass(A);
+    H_rec = interpolateColumnsAndHighPass(H);
 
-    L_rec = interpolateRowsAndLowPass(A_rec + H_rec,'same');
-    %figure; imagesc(L_rec);
+    L_rec = interpolateRowsAndLowPass(A_rec + H_rec);
 
-    D_rec = interpolateColumnsAndHighPass(D,'same');
-    V_rec = interpolateColumnsAndLowPass(V,'same');
+    D_rec = interpolateColumnsAndHighPass(D);
+    V_rec = interpolateColumnsAndLowPass(V);
 
-    H_rec = interpolateRowsAndHighPass(D_rec + V_rec,'same');
-    %figure; imagesc(H_rec);
-
+    H_rec = interpolateRowsAndHighPass(D_rec + V_rec);
     S = H_rec + L_rec;
 end
 
@@ -104,73 +191,55 @@ end
 
 end
 
-function output = interpolateRowsAndLowPass(input, valid)
+function output = interpolateRowsAndLowPass(input)
 lowPass = [1 1] / sqrt(2);
-output = interpolateAndConv(lowPass, input, 'rows', valid);
+output = interpolateRowsAndConv(lowPass, input);
 end
-function output = interpolateRowsAndHighPass(input, valid)
+function output = interpolateRowsAndHighPass(input)
 highPass = [-1 1] / sqrt(2);
-output = interpolateAndConv(highPass, input, 'rows', valid);
+output = interpolateRowsAndConv(highPass, input);
 end
-function output = interpolateColumnsAndLowPass(input, valid)
+function output = interpolateColumnsAndLowPass(input)
 lowPass = [1 1 ] / sqrt(2);
-output = interpolateAndConv(lowPass, input, 'columns', valid);
+output = interpolateColumnsAndConv(lowPass, input);
 end
-function output = interpolateColumnsAndHighPass(input, valid)
+function output = interpolateColumnsAndHighPass(input)
 highPass = [-1 1] / sqrt(2);
-output = interpolateAndConv(highPass, input, 'columns',valid);
+output = interpolateColumnsAndConv(highPass, input);
 end
 
-function output=interpolateAndConv(filter,data,Dim,valid)
-
-numRows = size(data,1);
-numColumns = size(data,2);
-
-if(Dim == "rows")
-
+function output=interpolateRowsAndConv(filter,data)
+    
+    numRows = size(data,1);
+    
     for row=1:numRows
         d_sym = data(row,:);
-        %d_sym(length(d_sym) + 1) = d_sym(length(d_sym)-1);
         d_sym = [d_sym(2) d_sym];
-
-
         IR = upsample(d_sym, 2);
         IR = conv(IR, filter, "valid");
-        %IR(length(IR)) = [];
-        %IR(length(IR) - 1) = [];
-        %IR = circshift(IR,1);
+
 
         IR(1) = [];
         output(row,:) = IR;
     end
-else
+end
+function output=interpolateColumnsAndConv(filter,data)
+
+    numColumns = size(data,2);
+
     for column=1:numColumns
         d_sym = data(:,column);
-        %d_sym(length(d_sym) + 1) = d_sym(length(d_sym)-1);
         d_sym = [d_sym(2); d_sym];
-
-
         IC = upsample(d_sym,2);
         IC = conv(IC, filter,  "valid");
-        %IC(length(IC)) = [];
-        %IC(length(IC) - 1) = [];
         IC(1) = [];
-        %IC = circshift(IC,1);
         
         output(:,column) = IC;
     end
 end
 
-end
 
-%         256         169   approx
-%         256         169   level 2
-%         511         338   level 1
-%        1022         676   original image
-
-%    256*169*4+511*338*3 = 691210
-
-% DWT Functions
+%% DWT Functions
 % output is stored as [A, Hn, Vn, Dn, ... , H1, V1, D1]
 function [output, D_out] = customDWT(image, level)
 output = [];
@@ -199,8 +268,6 @@ for l=1:level
     V_flat = reshape(V, [size(V,1) * size(V,2) 1]);
     D_flat = reshape(D, [size(D,1) * size(D,2) 1]);
     
-    %figure; imagesc(reshape(A_flat, [size(A,1) size(A,2)]));
-
     if(l == level)
         level_coef(l) = {[A_flat; H_flat; V_flat; D_flat]};
     else
@@ -216,40 +283,38 @@ end
 
 function output = lowPassRowsAndDecimate(input)
 lowPass = [1 1] / sqrt(2);
-output = convAndDecimate(lowPass, input, 'rows');
+output = convRowsAndDecimate(lowPass, input);
 end
 function output = lowPassColumnsAndDecimate(input)
 lowPass = [1 1] / sqrt(2);
-output = convAndDecimate(lowPass, input, 'columns');
+output = convColumnsAndDecimate(lowPass, input);
 end
 function output = highPassColumnsAndDecimate(input)
 highPass = [1 -1]/ sqrt(2);
-
-output = convAndDecimate(highPass, input,'columns');
+output = convColumnsAndDecimate(highPass, input);
 end
 function output = highPassRowsAndDecimate(input)
 highPass = [1 -1] / sqrt(2);
-output = convAndDecimate(highPass, input,'rows');
+output = convRowsAndDecimate(highPass, input);
 end
 
-function output=convAndDecimate(filter,data,Dim)
-
-numRows = size(data,1);
-numColumns = size(data,2);
-
-if(Dim == "rows")
-
-    for row=1:numRows
-        IR = conv(data(row,:), filter, "same");
-        IR = downsample(IR, 2);
-        output(row,:) = IR;
-    end
-else
+function output=convColumnsAndDecimate(filter,data)
+    numColumns = size(data,2);
+    
     for column=1:numColumns
         IC = conv(data(:,column), filter,"same");
         IC = downsample(IC,2);
         output(:,column) = IC;
     end
+
 end
+function output=convRowsAndDecimate(filter,data)
+    numRows = size(data,1);
+    
+    for row=1:numRows
+        IR = conv(data(row,:), filter, "same");
+        IR = downsample(IR, 2);
+        output(row,:) = IR;
+    end
 
 end
