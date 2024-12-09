da4a6b39e6185f47ad269e97fe0b4a6b30e7f9a3
Max Preissler <69917711+Maximilian21@users.noreply.github.com>
2024-12-09 15:53:25 -0800

    Update DSPProjectmerge.m

--- MATLAB/DSPProjectmerge.m
+++ MATLAB/DSPProjectmerge.m
@@ -584,25 +584,3 @@ for i = 1:m
 end
 
 end
-
-function Q = jpeg_qtable(quality)
-    % Standard JPEG quantization table
-    Q50 = [16 11 10 16 24 40 51 61;
-           12 12 14 19 26 58 60 55;
-           14 13 16 24 40 57 69 56;
-           14 17 22 29 51 87 80 62;
-           18 22 37 56 68 109 103 77;
-           24 35 55 64 81 104 113 92;
-           49 64 78 87 103 121 120 101;
-           72 92 95 98 112 100 103 99];
-    
-    if quality < 50
-        scale = 5000 / quality;
-    else
-        scale = 200 - 2 * quality;
-    end
-    
-    Q = floor((Q50 * scale + 50) / 100);
-    Q(Q < 1) = 1;
-    Q(Q > 255) = 255;
-end
